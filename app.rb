2fe8973b6d915187054e992ea2c17200c1fa7035
Katie Moosman <moosman.katie@gmail.com>
2019-03-08 03:59:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,13 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/'
-    erb :index
-  end
-
   configure do
     enable :sessions
     set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
   end
 
 end
